163d372ee0681edc3f3baeb900116d00060da973
Justin Bisignano <justinbis@fake.server>
2014-12-30 01:55:28 -0500

    Updated collections and router

--- server/publish.js
+++ server/publish.js
@@ -1,1 +1,29 @@
 // All Meteor publish functions should go here
+
+// Will become relevant once autopublish is gone
+
+// Allow access to all quiz seekers
+Meteor.publish('quizSeekers', function() {
+	return QuizSeekers.find({});
+});
+
+// Publish only the currently active quiz
+Meteor.publish('activeQuiz', function() {
+	// Make sure this person has a user ID (they are logged in)
+	if(this.userId)
+	{
+		return ActiveQizzes.find({
+			// Only publish those active quizzes for which this user is a player
+			// Make sure this user is one of the players
+			$or: [
+    			{ player1Id: this.userId },
+    			{ player2Id: this.userId }
+    		]
+		});	
+	}
+	else
+	{
+		// Otherwise, allow the page to proceed without publishing anything
+		this.ready();
+	}
+});
