17085405fd8a6685adf57810bb8bb9997e25291f
Justin Bisignano <justinbis@fake.server>
2015-03-22 15:53:48 -0400

    Added local gsap and usernames/icons on quiz page

--- client/templates/app-navbar.js
+++
@@ -1,8 +0,0 @@
-
-// For the login buttons, show an optional email feild
-if(Meteor.isClient)
-{
-	Accounts.ui.config({
-		passwordSignupFields: "USERNAME_AND_OPTIONAL_EMAIL"
-	});
-}
--- server/bootstrap.js
+++
@@ -1,4 +0,0 @@
-// if the database is empty on server start, we can create some sample data here
-Meteor.startup(function () {
-  return;
-});
