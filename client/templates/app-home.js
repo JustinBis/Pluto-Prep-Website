dcffac9ae09a4a66f35d2f872d388caa46732d52
Justin Bisignano <justinbis@fake.server>
2015-04-02 18:08:04 -0400

    Added add hope

--- client/templates/app-home.js
+++
@@ -1,6 +0,0 @@
-Template.appHome.events({
-	// When the take a quiz now button is clicked, redirect
-	"click #jumbotron-take-quiz-button": function () {
-		Router.go('quiz-select-subject');
-	}
-});
